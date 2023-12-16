308f351c765ea343e52b54481925d45f0a710289
Sohan Sarurkar <sohansarurkar47@gmail.com>
2023-12-16 15:38:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+// Use the router for handling routes
+app.use(router);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
